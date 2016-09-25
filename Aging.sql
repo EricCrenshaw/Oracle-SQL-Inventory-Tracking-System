fc9cc66d06ff196254753d913af8d3c7e10d6e4a
Eric L Crenshaw III <EricLCrenshaw3@gmail.com>
2016-09-25 16:44:28 -0700

    Update Aging.sql

--- Aging.sql
+++ Aging.sql
@@ -13,35 +13,23 @@ prompt
 
 accept vQdays prompt 'Please enter number of days to query: '
 
-
-
 column PO_Num heading 'PO|Number' format 9999
-
 column Order_Status heading 'PO|Stat' format a4
-
 column Date_Ordered heading 'Date|Open' format a10
-
 column Prod_Num heading 'Prod|Num' format a4
-
 column PD heading 'Product|Description' format a12
-
 column Order_Qty heading 'Order|Qty' format 999999
-
 column Unit_Price heading 'Unit|Price' format $999.99
-
 column OA heading 'Order|Amount' format $99999.99
-
 column Sup_Num heading 'Sup|Num' format a3
-
 column SN heading 'Supplier|Name' format a12
-
 column DO heading 'Days|Open' format 999
 
 set heading on
 set linesize 150
 
-
-select PO_Num, Order_Status, Date_Ordered, Products.Prod_Num, initcap(Prod_Desc) PD, Order_Qty, Unit_Price, Order_Qty*Unit_Price OA, Suppliers.Sup_Num, initcap(Sup_Name) SN, trunc(sysdate-Date_Ordered) +1 DO
+select PO_Num, Order_Status, 
+Date_Ordered, Products.Prod_Num, initcap(Prod_Desc) PD, Order_Qty, Unit_Price, Order_Qty*Unit_Price OA, Suppliers.Sup_Num, initcap(Sup_Name) SN, trunc(sysdate-Date_Ordered) +1 DO
 	from Purch_Orders, Suppliers, Products, Price_List, dual
 	where (trunc(sysdate-Date_Ordered) +1 >= &vQdays)
 	and Order_Status='O'
